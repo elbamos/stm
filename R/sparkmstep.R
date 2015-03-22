613afe3e41d8b96f9935ba28ef018d5bbec2043d
elbamos <amos.elberg@me.com>
2015-03-22 05:45:30 +0000

    beta file name

--- R/sparkmstep.R
+++ R/sparkmstep.R
@@ -179,7 +179,7 @@ mnreg.spark <- function(beta.ss,settings, spark.context, spark.partitions) {
       c.i = x
     )
   })
-  saveAsObjectFile(parallelize(spark.context, counts.list, spark.partitions), settings$betafile)
+  saveAsObjectFile(parallelize(spark.context, counts.list, spark.partitions), paste0(settings$betafile, round(rnorm(1) * 10000)))
   counts.rdd <- objectFile(spark.context, settings$betafile, spark.partitions)
 #  counts.rdd <- parallelize(spark.context, counts.list, spark.partitions)
   rm(counts.list)
